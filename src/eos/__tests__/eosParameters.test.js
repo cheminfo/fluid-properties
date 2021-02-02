92be7bc1dbade0985fb419d638463d646ac82987
Kevin M. Jablonka <kevin.jablonka@epfl.ch>
2021-02-02 17:58:26 +0100

    chore: fix failing test

--- src/eos/__tests__/eosParameters.test.js
+++ src/eos/__tests__/eosParameters.test.js
@@ -4,7 +4,7 @@ import { EOSParameters } from '../eosParameters.js';
 test('constructing EOS object', () => {
   const methane = MolecularFluid.fromName('methane');
   let eos = new EOSParameters(methane);
-  expect(eos.w).toStrictEqual(0);
-  expect(eos.u).toStrictEqual(1);
+  expect(eos.w).toStrictEqual(-1);
+  expect(eos.u).toStrictEqual(2);
   expect(eos.S).toBeGreaterThan(0);
 });
